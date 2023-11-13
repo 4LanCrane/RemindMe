1214faeccc5bd1929255c4e44dbc485c6580b7e6
4LanCrane <125448543+4LanCrane@users.noreply.github.com>
2023-11-13 18:45:49 +0000

    Move postion of buttons

--- app/src/main/java/com/example/remindme/MainActivity.java
+++ app/src/main/java/com/example/remindme/MainActivity.java
@@ -6,10 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.GridView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
